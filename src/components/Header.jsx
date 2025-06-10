1acf8a66c159f48e7786a4a304870cfa4a51646e
gabrieli <gabrielmolodini2@gmail.com>
2025-06-10 20:37:09 +0400

    replace font and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,11 +42,14 @@ function Header() {
           </li>
         </ul>
 
-        <i
+        {/* <i
           id="burger-menu"
           className="fa-solid fa-bars text-[1.7rem] cursor-pointer"
-        ></i>
-      </div>
+        ></i> */}
+
+          <svg id="burger-menu" className="  w-7 fill-amber-50 hover:fill-gray-200  cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+
+  </div>
     </header>
   );
 }
